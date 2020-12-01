e768c584a6d0c066e4945aabb62760e77ca4a5e3
FlaviusBortas <flaviusbortas@gmail.com>
2020-12-01 18:57:08 -0500

    Initial Commit

---
+++ AdventOfCode/AdventOfCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdventOfCodeApp.swift
+//  AdventOfCode
+//
+//  Created by Flavius on 12/1/20.
+//
+
+import SwiftUI
+
+@main
+struct AdventOfCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
